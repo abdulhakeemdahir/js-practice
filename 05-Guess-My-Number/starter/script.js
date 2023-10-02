998ff679f2fd28d7ce761fd4809085eecfc9f06d
Abdulhakeem Dahir <somlutions@gmail.com>
2023-10-02 15:18:07 -0700

    Add eventlistener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+// guessValue = 23;
+// console.log(guessValue);
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guessValue = document.querySelector('.guess').value;
+  console.log(guessValue);
+});
